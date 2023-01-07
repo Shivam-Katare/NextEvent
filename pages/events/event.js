90eccdd3b1d096b42e270cd05359f878b72ebd1b
Shivam Katare <91705825+Shivam-Katare@users.noreply.github.com>
2023-01-07 08:43:59 +0000

    Initial commit

---
+++ pages/events/event.js
@@ -0,0 +1,6 @@
+const Page = () => {
+  return (
+    <h1>Event One</h1>
+  )
+}
+export default Page;
---
+++ pages/index.js
@@ -0,0 +1,47 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container} >
+      <Head>
+        <title>Events App</title>
+        <meta name="description" content="next" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <header>
+        <nav>
+          <img />
+          <a href="/">Home</a>
+          <a href="/events">Events</a>
+          <a href="/about-us">About Us</a>
+        </nav>
+      </header>
+
+      <main className={styles.main}>
+        <a href="/">
+          <img />
+          <h2> Events In London </h2>
+          <p>lorem23 ytuu eieu nivnvnnvn ifijr jdnnd fhfhf rhfh vvh eooe ejejje kbkkb 3teb c jjgjgjjgj</p>
+        </a>
+
+          <a href="/india">
+          <img />
+          <h2> Events In India </h2>
+          <p>lorem23 ytuu eieu nivnvnnvn ifijr jdnnd fhfhf rhfh vvh eooe ejejje kbkkb 3teb c jjgjgjjgj</p>
+        </a>
+
+          <a href="/usa">
+          <img />
+          <h2> Events In USA </h2>
+          <p>lorem23 ytuu eieu nivnvnnvn ifijr jdnnd fhfhf rhfh vvh eooe ejejje kbkkb 3teb c jjgjgjjgj</p>
+        </a>
+      </main>
+
+      <footer className={styles.footer}></footer>
+
+    </div>
+  )
+}
